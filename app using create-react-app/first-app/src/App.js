e2e54dcb6485fe97e35f13cf825d0c144e72f70a
sumathi3399 <sumathivarma3399@gmail.com>
2022-11-21 12:21:06 +0530

    created react using create-react-app

---
+++ app using create-react-app/first-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="innerDiv">
+        <h1 className='heading'>Hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
